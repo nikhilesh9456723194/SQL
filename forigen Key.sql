c22a8fabd620d00fe07536fd3d69f46aeb9b4ad9
nikhilesh9456723194 <nikhilesh9456723194@gmail.com>
2026-03-25 21:47:00 +0530

    removed user viraj in foreign key sql file

--- forigen Key.sql
+++ forigen Key.sql
@@ -17,6 +17,5 @@ INSERT INTO student3
 (107, "Shyam", 28, 24000, "Rurdurpur", 76, "Male"),
 (108, "Virat", 29, 89000, "Haldwani", 77, "Male"),
 (109, "Viraj", 25, 65000, "Haldwani", 47, "Male"),
-(110, "Mahesh", 21, 56000, "Nanital", 43, "Male"),
-(111, "Vijay", 22, 43000, "Ruedurpr", 67, "Male");
+(110, "Mahesh", 21, 56000, "Nanital", 43, "Male");
 TRUNCATE TABLE student3 RESTART IDENTITY;
