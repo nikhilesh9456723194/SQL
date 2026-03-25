f28db4ab9eefc1d1612f668726d57664f666b680
nikhilesh9456723194 <nikhilesh9456723194@gmail.com>
2026-03-25 21:35:49 +0530

    adding foreign key sql file

---
+++ forigen Key.sql
@@ -0,0 +1,21 @@
+CREATE TABLE student3 (
+ID INT PRIMARY KEY,
+Name VARCHAR(40),
+Age INT CHECK (age>=18),
+Salary NUMERIC(10,2),
+City VARCHAR(25),
+Marks INT NOT NULL,
+Gender ENUM ('Male', 'Female')
+);
+INSERT INTO student3
+(ID, Name, Age, Salary, City, Marks, Gender) VALUES
+(101, "Rita", 32, 43000, "Almora", 56, "Female"),
+(102, "Sita", 34, 62000, "Haldwani", 87, "Female"),
+(103, "Gita", 25, 33000, "Almora", 98, "Female"),
+(105, "Niks", 33, 60000, "Almora", 45, "Male"),
+(106, "Ram", 23, 32000, "Nanital", 65, "Male"),
+(107, "Shyam", 28, 24000, "Rurdurpur", 76, "Male"),
+(108, "Virat", 29, 89000, "Haldwani", 77, "Male"),
+(109, "Mahesh", 21, 56000, "Nanital", 43, "Male"),
+(110, "Vijay", 22, 43000, "Ruedurpr", 67, "Male");
+TRUNCATE TABLE student3 RESTART IDENTITY;
